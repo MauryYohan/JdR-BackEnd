278aecb4427f644926038051cf2bb6f8bec766eb
Yohan Maury <mauryyohan1@gmail.com>
2018-06-28 10:11:53 +0200

    up

--- src/test/java/com/jdr/dao/UtilisateursDaoImplTest.java
+++ src/test/java/com/jdr/dao/UtilisateursDaoImplTest.java
@@ -8,24 +8,43 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.jdr.entity.Utilisateur;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 @Rollback(true)
 public class UtilisateursDaoImplTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
+	
+	@Autowired
+	UtilisateurDaoImpl userDao;
+	
+	Utilisateur u;
+	Utilisateur u1;
+	Utilisateur u2;
+	
 	@Before
 	public void setUp() throws Exception {
+		/* First Arguments = mail
+		 * Second Args = login
+		 * Third Args = pseudo		 
+		 * Fourth Args = motDePasse
+		 * string *4
+		 */
+		u = new Utilisateur("jdfkqj@hotmail.fr","Jaja", "abliblu", "4547");
+		u1 = new Utilisateur("aaaaaa@hotmail.fr","Baba", "valalia", "4673");
+		u2 = new Utilisateur("bbbbbb@hotmail.fr","Lala", "blablo", "3745");
+		
+		userDao.save(u);
+		userDao.save(u1);
+		userDao.save(u2);
 	}
-
+	
 	@Test
 	public void testFindAll() {
 		fail("Not yet implemented");
